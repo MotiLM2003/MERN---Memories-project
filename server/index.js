941091516514ead9da0fde0d5e48948ea152c77a
Moti Elmakies <motiphone2003@gmail.com>
2020-11-15 22:57:55 +0200

    fetching data

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ import cors from 'cors';
 
 const app = express();
 
-app.use('/posts/', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts/', postRoutes);
 
 import postRoutes from './routes/posts.js';
 
