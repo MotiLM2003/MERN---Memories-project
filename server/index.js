c8ffffbf05fe1e3c61834c93d212bc029c842f42
Moti Elmakies <motiphone2003@gmail.com>
2020-11-15 13:19:50 +0200

    intial commit

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use('/posts/', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+import postRoutes from './routes/posts.js';
+
+///   connecting to atlas
+const CONNECTION_URL =
+  'mongodb+srv://mernfulllstack:moti2003@cluster0.544zv.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// setting port
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('listening to port', PORT);
+    });
+  })
+  .catch((error) => {
+    console.log('error ---', error);
+  });
+
+mongoose.set('useFindAndModify', false);
+
+// app.listen(PORT, () => {
+//     console.log('liten');
+// });
