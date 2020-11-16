de145099352f0f40b561ca76f06ddeeac8b79e8c
Moti Elmakies <motiphone2003@gmail.com>
2020-11-16 17:11:53 +0200

    feature: add edit memory and readme.md

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,7 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
+// get all posts
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
@@ -10,15 +12,33 @@ export const getPosts = async (req, res) => {
   }
 };
 
+// create new post
 export const createPost = async (req, res) => {
   const post = req.body;
   const newPost = new PostMessage(post);
 
   try {
     const results = await newPost.save();
-    console.log(results);
+
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
+  }
+};
+
+// update post by ID
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).send('No post with thast id');
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).send('error updating');
   }
 };
